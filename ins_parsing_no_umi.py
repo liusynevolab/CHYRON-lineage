4de0c04be6e927d36ad34241ebb2a32bd9fb9aa3
liusynevolab <52087882+liusynevolab@users.noreply.github.com>
2019-08-22 15:12:39 -0700

    Add files via upload

---
+++ ins_parsing_no_umi.py
@@ -0,0 +1,101 @@
+import pickle
+import sys
+import pprint
+import re
+
+# with open('green_LT_insDict.pkl', 'rb') as pkl:
+# 	insDict = pickle.load(pkl)
+
+# insDict = {
+	# insertion:{		
+# 		"counts": {
+#			"well" : int()
+#		}
+# 	}
+# }
+## sysargv[1] = "exp"_well_data_file.txt
+def writestuff(file, numA, numC, numG, numT,combinations,combcount):
+	file.write('\n')
+	file.write(f"A: {numA}")
+	file.write('\n')
+	file.write(f"C: {numC}")
+	file.write('\n')
+	file.write(f"G: {numG}")
+	file.write('\n')
+	file.write(f"T: {numT}")
+	file.write('\n')
+	i = 0
+	while i < len(combinations):
+		file.write(f"{combinations[i]}: {combcount[i]}")
+		file.write('\n')
+		i += 1
+
+lengthcount = int(input("Enter the lengthcount: "))
+if lengthcount> 16:
+	lengthcount = 16
+if lengthcount< 1:
+	lengthcount = 1
+insDict = {}
+open('base_count_wells.txt','w').close()
+with open(sys.argv[1]) as samples:
+	for line in samples:
+		counter = 0
+		c = 0
+		count = 0
+		insCount = 0
+		line = line.strip().split(' ')
+		withlengths = line[0]
+		well_regex = re.search('(?<=_).*?(?=[.])', line[0]) ## finds well number between '_' and '.' of file name
+		well = f"well_{str(int(well_regex.group())+1)}" 
+		print(f"Working on {well}")
+
+		with open(withlengths) as lengths:
+			numA,numC,numG,numT = 0,0,0,0
+			combinations = ["AA","AC","AG","AT","CA","CC","CG","CT","GA","GC","GG","GT","TA","TC","TG","TT"]
+			combcount = [0] * 16
+			for line in lengths:
+				insInfo = line.strip().split('\t')
+				insertion = insInfo[1][:lengthcount] #slider1; the number after ":" should go from 1-15, the number after ":" indicates number of letters you want;
+				insLen = insInfo[2]
+				insCount = int(insInfo[3])
+				insPerc = insInfo[4]
+				numA += insertion.count('A')
+				numC += insertion.count('C')
+				numG += insertion.count('G')
+				numT += insertion.count('T')
+				i = 0
+				while i < len(combcount):
+					combcount[i] += insertion.count(combinations[i])
+					i += 1
+				#if insertion == 'ROOT':
+					#print("found one")
+				if insertion not in insDict: ## have we seen this insertion?
+					insDict[insertion] = {"counts":{well:insCount}}
+					c += 1
+					continue
+				if well in insDict[insertion]["counts"]: ## have we seen this insertion in this well?
+					insDict[insertion]["counts"][well] += insCount
+				else:
+					insDict[insertion]["counts"][well] = insCount
+			with open('base_count_wells.txt','a') as base_count:
+					base_count.write(f"{well}")
+					writestuff(base_count, numA, numC, numG, numT,combinations,combcount) #create a new text file and put stuff in it
+					#continue
+			# print(f"{c} unique insertions in {well}")
+			# print(f"{insCount} non-unique in {well}")
+
+countDict = {}
+
+for key in insDict:
+	for w in insDict[key]["counts"]:
+		if not w in countDict:
+			countDict[w] = 1
+		else:
+			countDict[w] += 1
+
+
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(countDict)
+
+with open('darkgreenLT_percinsDict.pkl', 'wb') as file:
+	pickle.dump(insDict, file)
---
+++ jaccard.py
@@ -0,0 +1,270 @@
+import pickle
+import pandas as pd
+from itertools import combinations, product
+import matplotlib.pyplot as plt
+from scipy.cluster import hierarchy
+from scipy.stats import norm
+import operator
+import numpy as np
+import tkinter as tk
+
+class Main:
+        def __init__(self,parent):
+                
+          
+           self.parent=parent
+           '''
+           self.geometry("200x200") 
+           self.minsize(100,100)
+           ''' 
+
+
+           
+           self.v2 = tk.IntVar()
+           self.v2.set(1)
+           self.v3 = tk.DoubleVar()
+           self.v3.set(0.0139)
+           self.v4 = tk.DoubleVar()
+           self.v4.set(1.0)
+
+                     
+           self.text1=tk.Label(self.parent,text="LENGTH CUTOFF")
+           self.text1=self.text1.place(x=0,y=0)
+          
+           self.text2=tk.Label(self.parent,text="COUNT CUTOFF")
+           self.text2=self.text2.place(x=0,y=20)
+           
+           
+           self.text3=tk.Label(self.parent,text="RECOVERY EFFICIENCY")
+           self.text3=self.text3.place(x=0,y=40)
+                        
+                        
+           self.entry0 = tk.Entry(self.parent,textvariable=str(self.v2))
+           self.entry0.place(x=130,y=0)
+          
+           
+           self.entry1 = tk.Entry(self.parent,textvariable=str(self.v3))
+           self.entry1.place(x=130,y=20)
+          
+           
+           self.entry2 = tk.Entry(self.parent,textvariable=str(self.v4))
+           self.entry2.place(x=130,y=40)
+           
+           
+             
+           B = tk.Button(self.parent, text = "QUIT", command = self.get)
+           B.place(x=50,y=70)
+           B.config(width=15,height=1,font=("Times New Roman",11))
+
+         
+           
+          
+           
+
+        def get(self):                
+
+            global val2, val3, val4 
+            val2,val3,val4=int(self.entry0.get()),float(self.entry1.get()),float(self.entry2.get())
+            self.parent.destroy() 
+
+
+with open('darkgreenLT_percinsDict.pkl', 'rb') as file:
+        insDict = pickle.load(file)
+
+root=tk.Tk()
+app = Main(root)
+root.mainloop()
+
+val3= val3*1000000
+
+
+LENGTH_CUTOFF = (val2,16) ## slider 2; the 2nd number should go from 2-16, inclusive, exclusive
+COUNT_CUTOFF = val3 #can we make this value a float rather than an integer? this would be slider 3
+RECOVERY_EFFICIENCY = val4 #slider 4, should go from 0.05-1
+
+#def update():
+#       global val2, val3, val4
+#       vel2 = e1.get()
+#       val3 = e2.get()*1000000
+#       val4 = e3.get()
+
+#master = tk.Tk()
+#tk.Label(master, text="LENGTH CUTOFF").grid(row=0)
+#tk.Label(master, text="COUNT CUTOFF(%)").grid(row=1)
+#tk.Label(master, text="RECOVERY EFFICIENCY").grid(row=2)
+#e1 = tk.Entry(master)
+#e2 = tk.Entry(master)
+#e3 = tk.Entry(master)
+#e1.insert(10,16)
+#e2.insert(10,0.0139)
+#e3.insert(10,1)
+
+#e1.grid(row=0, column=1)
+#e2.grid(row=1, column=1)
+#e3.grid(row=2, column=1)
+#tk.Button(master,text="Set",command=update()).grid(row=3)
+#tk.mainloop()
+
+
+def jaccard_calc(l1,l2):
+        a,b,c,d = (0,0,0,0)
+        for i in range(len(l1)):
+                if l1[i] == l2[i]:
+                        if l1[i] == 1:
+                                a+=1
+                                continue
+                        else:
+                                d+=1
+                                continue
+                else:
+                        if l1[i] == 1:
+                                b+=1
+                                continue
+                        else:
+                                c+=1
+                                continue
+        # print(a,b,c,d)
+        return (a)/(a+b+c)
+
+def insertionFinder(wellPairs, vectors):
+        for pair in wellPairs:
+                with open(f"{pair[0]}_{pair[1]}_insertions.txt",'w') as file:
+                        insertions = vectors["wells"][pair[0]][0].intersection(vectors["wells"][pair[1]][0])
+                        for insertion in insertions:
+                                file.write(insertion+'\n')
+
+def nextLevelInsertionFinder(wellPairs2, vectors):
+        for pair in wellPairs2:
+                with open(f"{pair[0]}_{pair[1]}_{pair[2]}_{pair[3]}_insertions.txt",'w') as file:
+                        set1 = vectors["wells"][pair[0]][0].intersection(vectors["wells"][pair[1]][0])
+                        set2 = vectors["wells"][pair[2]][0].intersection(vectors["wells"][pair[3]][0])
+                        insertions = set1.intersection(set2)
+                        for insertion in insertions:
+                                file.write(insertion+'\n')
+
+
+# c =0
+# for ins in insDict:
+#       print(ins, insDict[ins])
+#       c +=1
+#       if c >=10:
+#               break
+# exit()
+
+wells = []
+vectors = {}
+vectors["pool"] = [set(),[]]
+vectors["wells"] = {}
+
+group_1 = ["0","1","2","3","5","6","10","14"]
+group_2 = ["4","7","8","9","11","12","13","15"]
+
+for x in range(1,17):
+        wells.append(f"well_{str(x)}")
+        vectors["wells"][f"well_{str(x)}"] = [set(),[]]
+print(vectors["wells"][f"well_{str(x)}"])
+# for i in range(1,5):
+#       vectors["wells"][f"well_{str(i)}"] = [set(),[]]
+#       wells.append(f"well_{str(i)}")
+        # print(vectors["wells"])
+
+# for _ in group_2:
+#       vectors["wells"][f"well_{_}"] = [set(),[]]
+#       wells.append(f"well_{_}")
+
+
+wellPairs = [('well_7','well_16'), ('well_1','well_10'), ('well_2','well_11'), ('well_4','well_9'),
+                        ('well_15','well_3'), ('well_12','well_14'), ('well_13','well_6'), ('well_8','well_5')]
+
+wellPairs_2 = [('well_7','well_16', 'well_4','well_9'), ('well_2','well_11', 'well_1','well_10'),
+                        ('well_15','well_3', 'well_12','well_14'), ('well_13','well_6', 'well_5','well_8')]
+
+
+
+for ins in insDict:
+        if not ins == 'ROOT':
+                if len(ins) in range(*LENGTH_CUTOFF):
+                        for well in insDict[ins]["counts"]:
+                                if well in wells:
+                                        if insDict[ins]["counts"][well] >= COUNT_CUTOFF:
+                                                if np.random.choice([0,1], p=[1-RECOVERY_EFFICIENCY, RECOVERY_EFFICIENCY]):
+                                                        vectors["pool"][0].add(ins)
+                                                        vectors["wells"][well][0].add(ins)
+                                else:
+                                        print("couldnt find well")
+# nextLevelInsertionFinder(wellPairs_2, vectors)
+
+distDF = pd.DataFrame(columns=[x for x in wells], index=[x for x in wells], dtype='float64')
+
+
+for ins in vectors["pool"][0]:
+        for well in vectors["wells"]:
+                if ins in vectors["wells"][well][0]:
+                        vectors["wells"][well][1].append(1)
+                else:
+                        vectors["wells"][well][1].append(0)
+
+for well in vectors["wells"]:
+        print(f'{well} length {len(vectors["wells"][well][0])}')
+
+# for well in vectors["wells"]:
+        # print(f'{well} length {len(vectors["wells"][well][1])}')
+
+
+wellsAndDistances = {}
+for x,y in combinations(wells, 2):
+        d = jaccard_calc(vectors["wells"][x][1],vectors["wells"][y][1])
+        distDF.at[x, y] = d #round(d,1)
+        distDF.at[y, x] = d #round(d,1)
+        distDF.at[y, y] = 1
+        distDF.at[x, x] = 1
+        wellsAndDistances[x+'-'+y] = d
+
+orderedPairs = sorted(wellsAndDistances.items(), key=operator.itemgetter(1), reverse=True)
+seenWells = []
+final = []
+stderror = {}
+
+for wells,dist in orderedPairs:
+                well1,well2 = wells.split('-')
+                if well1 not in seenWells and well2 not in seenWells:
+                        final.append(wells) # Wells is the string of both wells
+                        seenWells.append(well1)
+                        seenWells.append(well2)
+                        col = [x for x in distDF.loc[well1] if x > 0]
+                        row = [y for y in distDF.loc[well2] if y > 0]
+                        row_col = []
+                        row_col.extend(row+col)
+                        # row_col.remove(dist)
+                        if len(col) > 1 and len(row) > 1:
+                                std = np.std(row_col)
+                                mean = np.mean(row_col)
+                                error = (dist-mean)/std
+                                stderror[wells] = error
+                        else:
+                                stderror[wells] = 0
+
+distDF.index=(['2-1-1','2-2-2','3-1-2','1-1-2','4-1-1','4-2-1','1-2-1','4-1-2','1-1-1','2-1-2','2-2-1','3-2-1','4-2-2','3-2-2','3-1-1','1-2-2'])
+print(distDF)
+print("**********Lowest Level***************")
+totalStd = 0
+for winnerWells in final:
+        print(f"{winnerWells} with a standard err of {stderror[winnerWells]}")
+        totalStd += stderror[winnerWells]
+print(f"Average Standard Err: {round(totalStd / (len(final)-1),2)}")
+print(f"Final confidence of {round(norm.cdf(totalStd / len(final)) * 100,2)}%")
+# if SYSTEM["SHOW_DENDROGRAM"]:
+# distDF = distDF.replace(np.nan,1)
+        # print(z)
+        # print(dn)
+        # plt.savefig("static/results.jpg")
+        # return (returnString,distDF)
+distDF.to_csv(f'jaccard_darkgreenLT_C{str(COUNT_CUTOFF)}_L{str(LENGTH_CUTOFF[0])}-{str(LENGTH_CUTOFF[1])}_Recovery_{str(int(RECOVERY_EFFICIENCY*100))}.csv')
+z = hierarchy.linkage(distDF, 'average') ##there are a few clustering choices here. for UPGMA, a standard algorithm, use 'average' instead of 'ward'
+# plt.figure(figsize=(14,6),dpi=100)
+dn = hierarchy.dendrogram(z, labels=distDF.index, leaf_rotation=90)
+
+
+
+
+plt.show()
